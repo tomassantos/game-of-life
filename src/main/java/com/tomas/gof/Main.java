c57d5f6e1ba2cea64f3649e0e2a5d2525895c59e
tomas <none@tomas.com>
2020-12-06 16:31:21 -0600

    Clean up main method.

--- src/main/java/com/tomas/gof/Main.java
+++ src/main/java/com/tomas/gof/Main.java
@@ -2,15 +2,11 @@ package com.tomas.gof;
 
 public class Main {
     public static void main(String[] args) {
-        int numberOfGenerations = 1;
-        int size = 3;
+        int numberOfGenerations = 5;
+        int size = 6;
         int seed = 4;
 
-        Universe universe = new Universe(size);
-//        universe.setLive(2,1);
-//        universe.setLive(2,2);
-//        universe.setLive(2,3);
-//        universe.setLive(1,2);
+        Universe universe = new Universe(size, seed);
         System.out.println(universe);
 
         Rule rule = new BasicRule();
@@ -18,7 +14,6 @@ public class Main {
         for (int i = 0; i < numberOfGenerations; i++) {
             rule.apply(universe);
             System.out.println(universe);
-            System.out.println(universe.toString().hashCode());
         }
     }
 }
